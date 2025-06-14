5488ba6d4976b803e001a503ecc024780d914dbb
mitoop <me@mitoop.com>
2025-06-14 11:23:55 +0800

    refactor
    Signed-off-by: mitoop <me@mitoop.com>

--- src/Client.php
+++ src/Client.php
@@ -7,33 +7,21 @@ use GuzzleHttp\ClientInterface;
 
 class Client
 {
-    protected string $mchid;
-
-    protected string $appid;
-
-    protected string $platformPrefix;
-
     protected ClientInterface $client;
 
     protected string $baseUrl;
 
-    protected RequestSigner $requestSigner;
+    protected RequestSigner $signer;
 
     /**
      * @param array{
-     *       mchid: string|int,
-     *       appid: string|int,
      *       base_url: string,
-     *       platform_prefix: string,
      *   } $config
      */
-    public function __construct(array $config, SignerInterface $signer, ?ClientInterface $client = null)
+    public function __construct(array $config, RequestSigner $signer, ?ClientInterface $client = null)
     {
-        $this->mchid = $config['mchid'];
-        $this->appid = $config['appid'];
         $this->baseUrl = rtrim($config['base_url'], '/');
-        $this->platformPrefix = strtoupper($config['platform_prefix']);
-        $this->requestSigner = new RequestSigner($this->mchid, $this->appid, $signer, $this->platformPrefix);
+        $this->signer = $signer;
         $this->client = $client ?: new GuzzleClient([
             'verify' => false,
             'http_errors' => false,
@@ -51,7 +39,7 @@ class Client
         return $this->client->post($url, [
             'json' => $data,
             'headers' => array_merge($headers, [
-                'Authorization' => $this->requestSigner->sign('POST', $this->fullUrl($url), $data),
+                'Authorization' => $this->signer->sign('POST', $this->fullUrl($url), $data),
             ]),
         ]);
     }
